dbff37dbcc1be73cb5b1ac61fa83339e16fbd554
Ryan Hardin <rhardin378@gmail.com>
2024-09-06 21:12:48 -0500

    added a searchbar component for layout purposes, styling changes to inventory view

--- ui/smoothie_portal/app/manager/inventory/page.js
+++ ui/smoothie_portal/app/manager/inventory/page.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import SidebarNavigation from "../../components/SidebarNavigation";
+import InventorySearchBar from "../../components/inventorySearch";
 import UserPanel from "../../components/userPanel";
 
 const Inventory = () => {
@@ -9,8 +10,13 @@ const Inventory = () => {
     <div className="flex">
       <SidebarNavigation />
       <div className="flex flex-col w-full">
-        <UserPanel />
-        <div>Inventory</div>
+        <div className="flex justify-between items-center px-4 py-3">
+          <InventorySearchBar />
+          <UserPanel />
+        </div>
+        <div className="text-3xl ml-4 px-4 py-2 font-mono font-bold">
+          Inventory
+        </div>
       </div>
     </div>
   );
